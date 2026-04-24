9f31590011658020880ac91afde54a30d7cfe185
Rendy <rendyzz999@gmail.com>
2026-04-24 18:46:08 +0700

    To Postgres

--- app/database.py
+++ app/database.py
@@ -1,11 +1,10 @@
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker, declarative_base
+import os
 
-DATABASE_URL = "sqlite:///./contacts.db"
+DATABASE_URL = os.getenv("postgresql://${{PGUSER}}:${{POSTGRES_PASSWORD}}@${{RAILWAY_PRIVATE_DOMAIN}}:5432/${{PGDATABASE}}")
 
-engine = create_engine(
-    DATABASE_URL, connect_args={"check_same_thread": False}
-)
+engine = create_engine("SQLALCHEMY_DATABASE_URL")
 
 SessionLocal = sessionmaker(bind=engine)
 
